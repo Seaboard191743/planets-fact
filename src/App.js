54ee2d1673ed2dae835723cf1728b6a49c60ba37
Usevalad Shychanin <exporesource@gmail.com>
2022-11-13 19:46:38 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import {
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+} from 'react-router-dom';
+
+import './app.css';
+
+// const router = createBrowserRouter(createRoutesFromElements());
+
+function App() {
+  // return <RouterProvider router={router} />;
+  return <h1>Template</h1>;
+}
+
+export default App;
