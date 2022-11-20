81c4356d1d791f8a4a524cacc0c22c51389f3857
Usevalad Shychanin <exporesource@gmail.com>
2022-11-20 12:47:21 +0300

    fix: info buttons active status switch

--- src/components/planetcontainer/planetinfo/PlanetInfo.jsx
+++ src/components/planetcontainer/planetinfo/PlanetInfo.jsx
@@ -8,7 +8,7 @@ export const PlanetInfo = ({ name, infoClass, handleClick, ...props }) => {
     Array.from(btnGroupRef.current.children).forEach(
       (elem) => (elem.style.background = 'none')
     );
-    if (e.target.className === btnGroupRef.current.className) {
+    if (e.target.className !== btnGroupRef.current.className) {
       e.target.style.background = props.selected;
     }
   };
